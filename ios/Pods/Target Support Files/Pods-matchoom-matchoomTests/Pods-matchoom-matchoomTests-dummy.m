ca60548bef5abbe1ae877edeb5d81d6e7cf7c489
Geon <geonulim@gmail.com>
2024-09-16 17:55:56 +0900

    init: 초기 설정

---
+++ ios/Pods/Target Support Files/Pods-matchoom-matchoomTests/Pods-matchoom-matchoomTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_matchoom_matchoomTests : NSObject
+@end
+@implementation PodsDummy_Pods_matchoom_matchoomTests
+@end
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View className='flex-1 items-center justify-center bg-green-200'>
+      <Text className='text-2xl font-bold'>MATCHOOM</Text>
+    </View>
+  );
+};
+
+export default App;
