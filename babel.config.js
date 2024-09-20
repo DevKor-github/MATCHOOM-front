d6d3092db3e27e4ef0d1155c53a09f8f31d7b16a
Geon <geonulim@gmail.com>
2024-09-20 17:12:55 +0900

    chore: path alias babel 설정

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,25 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['nativewind/babel'],
+  plugins: [
+    'nativewind/babel',
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@/navigation': './src/navigation',
+          '@/pages': './src/pages',
+          '@/components': './src/pages',
+          '@/features': './src/pages',
+          '@/services': './src/pages',
+          '@/utils': './src/pages',
+          '@/hooks': './src/pages',
+          '@/store': './src/pages',
+          '@/types': './src/pages',
+          '@/constants': './src/pages',
+          '@/styles': './src/pages',
+          '@/public': './src/pages',
+        },
+      },
+    ],
+  ],
 };
