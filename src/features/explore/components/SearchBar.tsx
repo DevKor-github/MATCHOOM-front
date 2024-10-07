7ebb93c7d1cb5770571e123020a6880cb70c0080
Geon <geonulim@gmail.com>
2024-10-07 17:40:36 +0900

    fix: 탐색 상단바 스크롤 시 고정되도록 수정

--- src/features/explore/components/SearchBar.tsx
+++ src/features/explore/components/SearchBar.tsx
@@ -6,14 +6,14 @@ const SearchBar = () => {
   const [value, setValue] = useState('');
 
   return (
-    <View className='relative flex-1'>
+    <View className='relative mb-4 h-11 w-full'>
       <TextInput
         onChangeText={setValue}
         value={value}
         placeholder='맞춤 클래스를 찾아보세요!'
         placeholderTextColor='#636872'
         textAlignVertical='center'
-        className='bg-grey-7 mb-4 flex h-11 flex-1 flex-row items-center rounded-[10px] py-0 pl-10 pr-3 text-[16px] font-medium leading-[20px] text-white'
+        className='bg-grey-7 flex h-full flex-row items-center rounded-[10px] py-0 pl-10 pr-3 text-[16px] font-medium leading-[20px] text-white'
       />
       <View className='absolute left-3 top-[10px]'>
         <SearchIcon />
--- src/pages/ExplorePage.tsx
+++ src/pages/ExplorePage.tsx
@@ -1,4 +1,4 @@
-import { ScrollView } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import ListCarousel from '@/features/explore/components/ListCarousel';
 import RectangleCarousel from '@/features/explore/components/RectangleCarousel';
 import SearchBar from '@/features/explore/components/SearchBar';
@@ -8,14 +8,16 @@ import { MOCK_CARDS } from '@/features/explore/mock/card';
 
 const ExplorePage = () => {
   return (
-    <ScrollView className='bg-background flex-1 px-5'>
+    <View className='bg-background flex-1 px-5'>
       <SearchBar />
       <Tabs />
-      <RectangleCarousel />
-      <ListCarousel title='HOT' cards={MOCK_CARDS} />
-      <SquareCarousel />
-      <ListCarousel title='꿀조합 컬렉션' cards={MOCK_CARDS} />
-    </ScrollView>
+      <ScrollView className='flex-1'>
+        <RectangleCarousel />
+        <ListCarousel title='HOT' cards={MOCK_CARDS} />
+        <SquareCarousel />
+        <ListCarousel title='꿀조합 컬렉션' cards={MOCK_CARDS} />
+      </ScrollView>
+    </View>
   );
 };
 
